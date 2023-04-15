d21c6de164b0940180febe4c2b3b635b4d478429
Varrience <105261606+Varrience@users.noreply.github.com>
2023-04-15 00:22:35 -0400

    fixing a property acess error in migration
    fixes a(n) property error when trying to access a property value with a - separator

--- scaler.js
+++ scaler.js
@@ -3,5 +3,5 @@ const element = document.getElementsByClassName("web")[0];
 //const styleTransform = element.style.transform;
 const aspect = Math.min(window.innerWidth, window.innerHeight);
 const scale = aspect / 400;
-element.style.transform = `translateX(-50%) scale(${scale})`;
-element.style.transfrom-origin = `top center`;
+element.style["transform"] = `translateX(-50%) scale(${scale})`;
+element.style["transfrom-origin"] = `top center`;
