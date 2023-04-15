c52cc1d90d4e6e0585c878f251d6d1f895564e63
Varrience <105261606+Varrience@users.noreply.github.com>
2023-04-15 00:11:19 -0400

    migrating all upscaling styling to this script
    you know so people don't go hunting for it i hate that as much as you do

--- scaler.js
+++ scaler.js
@@ -1,6 +1,7 @@
 // Scales the current project to the full screen without streching
 const element = document.getElementsByClassName("web")[0];
-const styleTransform = element.style.transform;
+//const styleTransform = element.style.transform;
 const aspect = Math.min(window.innerWidth, window.innerHeight);
-const scale = aspect / 400 - 0.05;
-element.style.transform = `${styleTransform} scale(${scale})`;
+const scale = aspect / 400;
+element.style.transform = `translateX(-50%) scale(${scale})`;
+element.style.transfrom-origin = `top center`;
