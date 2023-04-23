f7105140b8e1a69c2912c664549291d8b5d7c87d
Varrience <105261606+Varrience@users.noreply.github.com>
2023-04-23 02:03:55 -0400

    Update scaler.js

--- scaler.js
+++ scaler.js
@@ -2,6 +2,7 @@
 const element = document.getElementsByClassName("web")[0];
 const aspect = Math.min(window.innerWidth, window.innerHeight);
 const scale = aspect / element.style["width"]; // divide this by the height if you want the joypad
+element.style["background"] = "black";
 element.style["left"] = "50%";
 element.style["transform"] = `translateX(-50%) scale(${scale})`;
 element.style["transform-origin"] = `top center`;
