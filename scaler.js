cba4a9efb82c2c4c16b6901ed9deaa0269eb62a3
Varrience <105261606+Varrience@users.noreply.github.com>
2023-04-23 02:01:58 -0400

    tidying up and making it scalable

--- scaler.js
+++ scaler.js
@@ -1,8 +1,7 @@
 // Scales the current project to the full screen without streching
 const element = document.getElementsByClassName("web")[0];
-//const styleTransform = element.style.transform;
 const aspect = Math.min(window.innerWidth, window.innerHeight);
-const scale = aspect / 400;
+const scale = aspect / element.style["width"]; // divide this by the height if you want the joypad
 element.style["left"] = "50%";
 element.style["transform"] = `translateX(-50%) scale(${scale})`;
 element.style["transform-origin"] = `top center`;
