ff823d7e1f3713cb8b628f675c58db9763d0f6fa
Varrience <105261606+Varrience@users.noreply.github.com>
2023-04-23 01:54:28 -0400

    moving all modifications of index html to here

--- scaler.js
+++ scaler.js
@@ -3,5 +3,6 @@ const element = document.getElementsByClassName("web")[0];
 //const styleTransform = element.style.transform;
 const aspect = Math.min(window.innerWidth, window.innerHeight);
 const scale = aspect / 400;
+element.style["left"] = "50%";
 element.style["transform"] = `translateX(-50%) scale(${scale})`;
 element.style["transform-origin"] = `top center`;
